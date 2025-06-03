9e68450c3400515acff25a0d8ecee47585be18c9
Viktor Kyshenchuk <viktor.kyshenchuk@gmail.com>
2025-06-03 22:37:26 +0300

    Check

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,6 +5,11 @@ const rows = table.querySelectorAll('tr');
 
 rows.forEach((row) => {
   const cells = row.querySelectorAll('th, td');
+
+  if (cells.length < 2) {
+    return;
+  }
+
   const secondCell = cells[1];
   const lastCell = cells[cells.length - 1];
 
